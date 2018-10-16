20dcc3a07a9d795b0f137ecc739ec2673e3f9ac7
Dang <lyhoangdang96@gmail.com>
2018-10-16 13:32:49 +0700

    add more images of artists

--- js/app.js
+++ js/app.js
@@ -1,22 +1,25 @@
 $(document).ready(function () {
+    
     $("header").load("../shared/header.html");
     $("footer").load("../shared/footer.html");
 });
-$(document).ready(function () {
-    $.getJSON('../json/artist.json',function(data){
-        alert('success');
-        $.each(data.artists,function(i,art){
-            $('#artist-list').append(
-                '<div class="card">' +
-                    '<img class="card-img-top" src="' + art.img + '" alt="Card image cap">' + 
-                    '<div class="card-body">' +
-                        '<h5 class="card-title">' + art.name + '</h5>' +
-                        '<p class="card-text">' + art.desc + '</p>' +
-                        '<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>' +
-                    '</div></div>'
-                );
-        });
-    }).error(function(){
-        alert('error');
-    });
-});
+// $(function () {
+//     alert("json");
+//     $.getJSON('../json/artist.json',function(data){
+//         alert('success');
+//         $.each(data.artists,function(i,art){
+//             $('#artist-list').append(
+//                 '<div class="card">' +
+//                     '<img class="card-img-top" src="' + art.img + '" alt="Card image cap">' + 
+//                     '<div class="card-body">' +
+//                         '<h5 class="card-title">' + art.name + '</h5>' +
+//                         '<p class="card-text">' + art.desc + '</p>' +
+//                         '<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>' +
+//                     '</div></div>'
+//                 );
+//         });
+//     }).error(function(){
+//         alert('error');
+//     });
+// });
+
